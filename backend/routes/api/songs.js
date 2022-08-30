56caca0bcc8b7850cb089adc40871a2cad4fcaff
Joanna <gilbert.joannac@gmail.com>
2022-08-30 11:53:17 -0600

    GET /current route that gets all songs created by the current user

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,15 +1,63 @@
-const express = require('express')
-const { Song } = require('../../db/models')
-// const { check } = require('express-validator');
-// const { setTokenCookie, requireAuth } = require('../../utils/auth');
-// const { handleValidationErrors } = require('../../utils/validation');
+const express = require('express');
+const { Song, User } = require('../../db/models');
+const { check } = require('express-validator');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
 // Get all songs
 router.get('/', async (req, res) => {
     const songs = await Song.findAll();
-    res.json(songs);
+    return res.json(songs);
 });
+
+// Get all Songs created by the Current User
+// Authentication: true
+router.get('/current', restoreUser, async (req, res) => {
+    const { user } = req;
+    if (user) {
+        const songs = await Song.findAll({
+            where: { userId: user.id }
+        });
+        return res.json({ songs });
+    } else return res.json({
+        "message": "Authentication required",
+        "statusCode": 401
+    });
+});
+
+// All endpoints that require authentication 
+// and the current user does not have the
+// correct role(s) or permission(s).
+
+// * Request: endpoints that require proper authorization
+// * Error Response: Require proper authorization
+//   * Status Code: 403
+//   * Headers:
+//     * Content-Type: application/json
+//   * Body:
+
+//     ```json
+//     {
+//       "message": "Forbidden",
+//       "statusCode": 403
+//     }
+//     ```
+// All endpoints that require a current user to be logged in.
+
+// * Request: endpoints that require authentication
+// * Error Response: Require authentication
+//   * Status Code: 401
+//   * Headers:
+//     * Content-Type: application/json
+//   * Body:
+
+//     ```json
+//     {
+//       "message": "Authentication required",
+//       "statusCode": 401
+//     }
+//     ```
 
 module.exports = router;
