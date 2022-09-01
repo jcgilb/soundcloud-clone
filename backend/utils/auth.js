c059c966a9fc10c1fd533158680d5074c9ce6f79
Joanna <gilbert.joannac@gmail.com>
2022-09-01 12:48:44 -0600

    fixed sign-up and login responses to have a token

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -12,9 +12,7 @@ const setTokenCookie = (res, user) => {
         secret,
         { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
     );
-
     const isProduction = process.env.NODE_ENV === "production";
-
     // Set the token cookie
     res.cookie('token', token, {
         maxAge: expiresIn * 1000, // maxAge in milliseconds
@@ -22,7 +20,6 @@ const setTokenCookie = (res, user) => {
         secure: isProduction,
         sameSite: isProduction && "Lax"
     });
-
     return token;
 };
 
@@ -30,12 +27,10 @@ const restoreUser = (req, res, next) => {
     // token parsed from cookies
     const { token } = req.cookies;
     req.user = null;
-
     return jwt.verify(token, secret, null, async (err, jwtPayload) => {
         if (err) {
             return next();
         }
-
         try {
             const { id } = jwtPayload.data;
             req.user = await User.scope('currentUser').findByPk(id);
@@ -43,9 +38,7 @@ const restoreUser = (req, res, next) => {
             res.clearCookie('token');
             return next();
         }
-
         if (!req.user) res.clearCookie('token');
-
         return next();
     });
 };
@@ -53,7 +46,6 @@ const restoreUser = (req, res, next) => {
 // If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
-
     const err = new Error('Unauthorized');
     err.title = 'Unauthorized';
     err.errors = ['Unauthorized'];
