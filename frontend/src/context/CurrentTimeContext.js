b34ec0826d32f24457237e8c3a3315d6dc800708
Joanna <gilbert.joannac@gmail.com>
2022-10-12 17:29:41 -0600

    dont need currentTimeContext

--- frontend/src/context/CurrentTimeContext.js
+++ frontend/src/context/CurrentTimeContext.js
@@ -1,19 +1,19 @@
-import { createContext, useContext, useState } from "react";
+// import { createContext, useContext, useState } from "react";
 
-export const CurrentTimeContext = createContext();
-export const useCurrentTime = () => useContext(CurrentTimeContext);
+// export const CurrentTimeContext = createContext();
+// export const useCurrentTime = () => useContext(CurrentTimeContext);
 
-export default function CurrentTimeProvider(props) {
-  const [curTime, setCurTime] = useState(0);
+// export default function CurrentTimeProvider(props) {
+//   const [curTime, setCurTime] = useState(0);
 
-  return (
-    <CurrentTimeContext.Provider
-      value={{
-        curTime,
-        setCurTime,
-      }}
-    >
-      {props.children}
-    </CurrentTimeContext.Provider>
-  );
-}
+//   return (
+//     <CurrentTimeContext.Provider
+//       value={{
+//         curTime,
+//         setCurTime,
+//       }}
+//     >
+//       {props.children}
+//     </CurrentTimeContext.Provider>
+//   );
+// }
