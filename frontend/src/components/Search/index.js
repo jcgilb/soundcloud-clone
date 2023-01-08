71abf30c9105bce63e11bfc70dc26b8d0b58dfe8
Joanna <gilbert.joannac@gmail.com>
2023-01-07 19:13:46 -0700

    search component

---
+++ frontend/src/components/Search/index.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import "./Search.css";
+
+const Search = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  return <></>;
+};
+
+export default Search;
