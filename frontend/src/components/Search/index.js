01a9d041b21a217b9249c6300072db0969dafbe8
Joanna <gilbert.joannac@gmail.com>
2023-01-08 18:06:25 -0700

    search items

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import "./Search.css";
 
 const Search = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const songnames = useSelector((state) => state.songs);
   return <></>;
 };
 
